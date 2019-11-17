b3e941b472a70effe122283c5dcfdcb9ca636581
Jamal Rjoul <jamensky1991@gmail.com>
2019-11-17 20:37:35 +0000

    Automatically backed up by Learn

--- lib/hello.rb
+++ lib/hello.rb
@@ -1,6 +1,23 @@
-def hello_t
 
+def hello_t(array)
+  i = 0
+  while i < array.length
+  yield array[i]
+    i = i + 1
+  end
 end
 
-# call your method here!
 
+
+hello_t(array = ["Tim", "Tom", "Jim"]) do |name|
+  x = []
+  array.collect do |n|
+  if name.start_with?("T")
+     x.push ("Hi, #{n}")
+   end
+   puts x
+  end
+end
+
+
+hello_t(["Tim", "Tom", "Jim"]) {|name| puts name}
